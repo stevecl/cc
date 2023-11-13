6cccda1a137efbca861f3eda043554117a69062b
cc <chenglian@maytek.cn>
2023-11-13 10:16:05 +0800

    test commit

--- cc_web/src/main.js
+++ cc_web/src/main.js
@@ -6,6 +6,12 @@ import router from './router'
 import './assets/reset.less'
 import './assets/base.less'
 
-createApp(App)
+let app = createApp(App)
   .use(router)
   .mount('#app')
+
+
+  app.config.errorHandler = (err) => {
+    /* 处理错误 */
+    console.log('error: ', err)
+  }
