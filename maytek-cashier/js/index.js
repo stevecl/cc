d506516b70daf2ff53caed0b12eff43a2d4852f9
cc <1040457762@qq.com>
2024-03-31 22:56:35 +0800

    test proxy

---
+++ maytek-cashier/js/index.js
@@ -0,0 +1,48 @@
+import './rem.js'
+
+const { createApp } = Vue
+
+function queryParse(query){
+  let queryText = "";
+  for(let key in query){
+    queryText += `${key}=${query[key]}&`;
+  }
+  return queryText.slice(0, -1);
+};
+
+createApp({
+  setup() {
+    const handleLogin = async () => {
+      console.log('login')
+      let clientId = '428a8310cd442757ae699df5d894f051'
+      let res = await fetch("/prod-api/auth/login", {
+        method: "POST",
+        headers: {
+          // "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          clientId,
+          grantType: "passwordApp",
+          username: "xinchong",
+          password:"123456"
+        })
+      })
+      res.json().then(json => {
+        let { code, data, msg } = json
+        let { access_token } = data
+        localStorage.setItem('token', access_token)
+        localStorage.setItem('clientId', clientId)
+        console.log('json:', access_token)
+        location.href = './cashier.html'
+      })
+    }
+    return {
+      message: 'sadasssd',
+      handleLogin
+    }
+  }
+
+}).mount('#app')
+
+console.log('createApp', createApp)
