86505dfc3616e8a14955b91b632dae479b421d40
cc <1040457762@qq.com>
2024-09-11 00:26:55 +0800

    modify

---
+++ ph/src/material/goods/config.js
@@ -0,0 +1,61 @@
+let config = {
+  title: '商品组',
+  style: {
+    background: '#ffffff', // 底部背景
+    padding: '0 0 0 0',
+    margin: '0 0 0 0',
+    goodBgColor: '#fff', // 商品背景
+  },
+  showType: 1,
+  detailConfig: {
+    title: {
+      show: true,
+      name: '商品标题',
+      style: {
+        color: '#37383A',
+        resetColor: '#37383A'
+      }
+    },
+    subtitle: {
+      show: true,
+      name: '副标题',
+      style: {
+        color: '#999CA7',
+        resetColor: '#999CA7'
+      }
+    },
+    price: {
+      show: true,
+      name: '商品价格',
+      style: {
+        color: '#FF2F46',
+        resetColor: '#FF2F46',
+      }
+    },
+    oldPrice: {
+      show: false,
+      name: '划线原价',
+      style: {
+        color: '#999CA7',
+        resetColor: '#999CA7',
+      }
+    },
+    sales: {
+      show: false,
+      name: '商品销量',
+      style: {
+        color: '#999CA7',
+        resetColor: '#999CA7',
+      }
+    },
+    tag: {
+      show: true,
+      name: '标题标签',
+    }
+  },
+  datas: [
+    
+  ]
+}
+
+export default config
---
+++ ph/src/material/goods/config.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="module-edit-title">边距设置</div>
+    <configItem :label="'顶部间距'">
+      <el-slider style="width: 250px;" color="red" v-model="marginTop" :min="-100" :max="100" :step="1" :show-tooltip="false"></el-slider>
+      <span class="slide-span--unit">{{ marginTop }}<span>px</span></span>
+    </configItem>
+    <configItem :label="'上边距'" :labelWidth="70">
+      <inputNumber v-model="paddingTop"></inputNumber>
+      <div class="label2 wid70">左右边距(内)</div>
+      <inputNumber v-model="paddingLeft"></inputNumber>
+    </configItem>
+    <configItem :label="'下边距'" :labelWidth="70">
+      <inputNumber v-model="paddingBottom"></inputNumber>
+      <div class="label2 wid70">左右边距(外)</div>
+      <inputNumber v-model="marginLeft"></inputNumber>
+    </configItem>
+    
+    <div class="module-edit-title">组件设置</div>
+    <configItem :label="'底部背景'">
+      <selectColor v-model="config.style.background" :resetColor="'#fff'"></selectColor>
+      <div class="label2 wid70">商品背景</div>
+      <selectColor v-model="config.style.goodBgColor"></selectColor>
+    </configItem>
+    <configItem :label="'选择风格'">
+      <el-radio-group v-model="config.showType">
+        <el-radio :value="1">单列展示</el-radio>
+        <el-radio :value="2">双列展示</el-radio>
+      </el-radio-group>
+    </configItem>
+
+    <div class="module-edit-title">显示内容</div>
+    <div class="content">
+      <div class="content_item" v-for="(item, key) in config.detailConfig">
+        <el-checkbox v-model="item.show"></el-checkbox>
+        <div class="name" @click="item.show = !item.show">{{ item.name }}</div>
+        <selectColor v-model="item.style.color" :resetColor="item.style.resetColor" v-if="key !== 'tag'"></selectColor>
+      </div>
+    </div>
+
+    <div class="module-edit-title">选择商品 </div>
+    <configItem :label="'展示商品'">
+      <el-radio-group v-model="config.showType">
+        <el-radio :value="1">手动选择</el-radio>
+        <el-radio :value="2">商品分类</el-radio>
+      </el-radio-group>
+    </configItem>
+  </div>
+</template>
+
+<script setup>
+import useStyle from '@/hooks/useStyle';
+
+const props = defineProps({
+  config: Object
+})
+
+const { marginTop, marginLeft, paddingTop, paddingLeft, paddingBottom } = useStyle(props)
+
+</script>
+
+<style lang="scss" scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  &_item {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+    padding-left: 18px;
+    .name {
+      width: 100px;
+      margin-left: 4px;
+      cursor: pointer;
+    }
+  }
+}
+</style>
