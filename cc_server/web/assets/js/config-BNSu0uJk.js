16f0a6c1084cbd802b4ad672e0b4c313324f65d5
cc <chenglian@maytek.cn>
2024-09-29 10:35:09 +0800

    feat

---
+++ cc_server/web/assets/js/config-BNSu0uJk.js
@@ -0,0 +1,1 @@
+let e={title:"单图组",style:{bgColor:"#ededed",bgImageUrl:"",padding:"0 0 0 0",margin:"0 0 0 0"},datas:[{id:0,defaultIcon:"default_banner.png",url:"",link:""},{id:1,defaultIcon:"default_banner.png",url:"",link:""}]};export{e as default};
